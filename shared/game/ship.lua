e854fd52a604b19de136fc704605affa80189f0f
Kyle Emmerich <emmerich.kyle@gmail.com>
2015-12-18 02:07:55 -0600

    Ship now uses ShipHull as renderable

--- shared/game/ship.lua
+++ shared/game/ship.lua
@@ -1,8 +1,10 @@
 local class = require 'shared.class'
 local vec2 = require 'shared.core.vec2'
+local color = require 'shared.core.color'
 local Object = require 'shared.core.object'
 local ShipHull = require 'shared.game.shiphull'
 local ShipComponent = require 'shared.game.shipcomponent'
+
 local ffi = require 'ffi'
 
 ffi.cdef [[
@@ -13,22 +15,40 @@ typedef struct {
 	ID hull_type;
 	string name;
 	array components;
+
+	color custom_color;
 } Ship_data;
 
 ]]
 
 local Ship = class('Ship', Object)
 Ship.data = ffi.typeof('Ship_data')
-function Ship:initialize(hull)
+function Ship:initialize()
 	Object.initialize(self, vec2(0, 0), vec2(0, 0), 0, 0)
-	self.hull = hull
+	self.hull = false
+	self.renderable = false
 	self.components = {}
+
+	self.custom_color = color{ 255, 255, 255, 255 }
 end
 
 function Ship:AddComponent(component)
 	table.insert(self.components, component)
 	component.ship = self
 end 
+
+function Ship:SetHull(hull)
+	self.hull = hull
+	self.renderable = hull
+
+	self:RecalculateMass()
+end
+
+function Ship:RecalculateMass()
+	self.mass = self.hull.mass
+
+	--add component masses
+end
 
 function Ship:Serialize(serializer)
 	local data = Ship.data()
@@ -39,14 +59,18 @@ function Ship:Serialize(serializer)
 
 	data.components = serializer:PutArray(ShipComponent, self.components)
 
+	data.custom_color = self.custom_color
+
 	return data
 end
 
 function Ship:Deserialize(serializer, data)
 	Object.Deserialize(self, serializer, data.object)
-	self.hull = ShipHull.static.hulls[data.hull_type]
+	self:SetHull(ShipHull.static.hulls[data.hull_type])
 
 	self.name = serializer:GetString(data.name)
+
+	self.custom_color = data.custom_color
 
 	local components = serializer:GetArray(ShipComponent, data.components)
 	for i, v in ipairs(components) do
