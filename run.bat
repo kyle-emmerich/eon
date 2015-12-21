41bd3cfa119a514dc3c007a1e5af0a9019f2d4b3
Kyle Emmerich <emmerich.kyle@gmail.com>
2015-12-21 10:01:41 -0600

    no message

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 set LOVEPATH="C:\Program Files (x86)\LOVE\love.exe"
-set EONDIR="Z:\Development\LOVE\eon"
+set EONDIR="C:\Users\Kyle\Documents\eon"
 echo Starting the server...
 start "Eon Server" %LOVEPATH% %EONDIR% server
 echo Starting the client.
